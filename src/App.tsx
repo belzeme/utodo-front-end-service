e60de53d17f8ebd8fcfd860c7f04d4dbb07e2dc0
clement belvisee <clement.belvisee@gmail.com>
2023-04-05 20:22:16 +0000

    init MUI library

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+import { api } from "./api/todos";
+import ToDoListView from "./views/todos.view/TodoListView";
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+
+  const pingService = async () => {
+    const isAvailable = await api.ping()();
+    setIsConnected(isAvailable);
+  };
+
+  const checkTodoApisConnection = () => {
+    pingService();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button variant="contained" onClick={checkTodoApisConnection}>
+          Check for backend connection
+        </Button>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <span>isConnected:</span>
+          <span>{isConnected ? "yes" : "no"}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ToDoListView />
       </header>
     </div>
   );
